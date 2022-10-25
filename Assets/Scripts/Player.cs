7413e3a3e6c57f2d3ac0c85cadb147cbd9a0bee7
29-CheongChinWei <115056022+29-CheongChinWei@users.noreply.github.com>
2022-10-25 13:20:50 +0800

    added jumping to player and resrart sene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public float velocity = 1;
+    private Rigidbody rb;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            thisAnimation.Play();
+            rb.velocity = Vector2.up * velocity;
+        thisAnimation.Play();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene("RestartScene");
     }
 }
